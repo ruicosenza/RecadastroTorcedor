0cfe6d3011b2a143ac69cc935d8b0731aea149f1
Rui Cosenza <ruicosenza@gmail.com>
2018-08-08 16:05:06 -0300

    Acertando a comunicação entre os webservices

--- src/main/java/entity/Socio.java
+++ src/main/java/entity/Socio.java
@@ -20,4 +20,12 @@ public class Socio {
     public void setId(int id) {
         this.id = id;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,22 +1,15 @@
 package hello;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import entity.Response;
 import entity.Socio;
-import org.json.JSONObject;
+
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -37,41 +30,16 @@ public class GreetingController {
     }
 
     @RequestMapping( value = "/", produces = (MediaType.APPLICATION_JSON_VALUE))
-    public JSONObject testando(){
-        /*RestTemplate restTemplate = new RestTemplate();
-        Socio forObject = restTemplate.getForObject("http://localhost:8080/teste", Socio.class);
+    public ResponseEntity<Socio[]> testando(){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Socio[]> forEntity = restTemplate.getForEntity("http://localhost:8080/teste", Socio[].class);
 
-        System.out.println(forObject);*/
-        JSONObject jsonObj = new JSONObject();
-        BufferedReader br;
-        try {
-            URL url = new URL("http://localhost:8080/teste");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
+        Socio[] socios = forEntity.getBody();
 
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "+ conn.getResponseCode());
-            }
-
-            br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-                jsonObj = new JSONObject(output.replace("[", "").replace("]", ""));
-            }
-
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < socios.length; i++) {
+            System.out.println(socios[i].getNome());
         }
-        return jsonObj;
+
+        return forEntity;
     }
 }
